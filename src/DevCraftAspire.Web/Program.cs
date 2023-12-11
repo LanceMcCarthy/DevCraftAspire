b31c2f1851c82dfcf2117b51456cf6791fc05653
JustinR34 <89593857+JustinR34@users.noreply.github.com>
2023-12-11 17:02:53 -0500

    NativeBlazorReportViewer

--- src/DevCraftAspire.Web/Program.cs
+++ src/DevCraftAspire.Web/Program.cs
@@ -11,6 +11,8 @@ builder.AddRedisOutputCache("cache");
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddTelerikBlazor();
+
 
 builder.Services.AddHttpClient<WeatherApiClient>(client=> client.BaseAddress = new("http://apiservice"));
 builder.Services.AddHttpClient<ReportsApiClient>(client=> client.BaseAddress = new("http://apiservice"));
