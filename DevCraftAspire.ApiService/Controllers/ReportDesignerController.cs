d398317c7ac53b882c67c5be98e7a134fed7d083
JustinR34 <89593857+JustinR34@users.noreply.github.com>
2025-11-07 17:17:46 -0500

    Pushing some stuffs

---
+++ DevCraftAspire.ApiService/Controllers/ReportDesignerController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Telerik.Reporting.Services;
+using Telerik.WebReportDesigner.Services;
+using Telerik.WebReportDesigner.Services.Controllers;
+[Route("api/reportdesigner")]
+[ApiController]
+public class ReportDesignerController : ReportDesignerControllerBase
+{
+    public ReportDesignerController(IReportDesignerServiceConfiguration reportDesignerServiceConfiguration, IReportServiceConfiguration reportServiceConfiguration)
+        : base(reportDesignerServiceConfiguration, reportServiceConfiguration)
+    {
+    }
+}
